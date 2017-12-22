dbbb08235364cbb6632ef6cbcdf7f52040e4886c
Nir's Yoga <getter@post.bgu.ac.il>
2017-12-22 10:23:48 +0200

    add delete function

--- registration.py
+++ registration.py
@@ -95,9 +95,8 @@ def addDate(new_date):
 def deleteOperation(del_date):
     nds = del_date.split("-") #new_date split
     ndate = dt( int(nds[0]), int(nds[1]), int(nds[2]) )
-    op = mdb['operations'].find({"date": ndate})
-    op.remove()
-    return jsonify(data=str(op), success=True)
+    op = mdb['operations'].find_one({"date": ndate})
+    return jsonify(data=str(op['date']), success=True)
 
 
 @app.route('/index')
