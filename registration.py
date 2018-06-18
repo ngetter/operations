b30fae25f95f0a25307ce1821a217a4c8d401855
Nir Getter <ngetter@gmail.com>
2018-06-18 05:45:14 +0000

    batchdates

--- registration.py
+++ registration.py
@@ -25,7 +25,7 @@ from premailer import transform
 ###### configuration
 try:
     LOCAL = os.getenv('C9_HOSTNAME')=="scheduleme-ngetter.c9users.io"# Make true if running from the IDE
-    MONGO_LOCAL = LOCAL and True # Make true if running from the IDE and the mongod is running too on the local machine
+    MONGO_LOCAL = True and True # Make true if running from the IDE and the mongod is running too on the local machine
     RETURN_TO = os.getenv('IP')
 except Exception:
     LOCAL = False
@@ -78,6 +78,10 @@ def serviceworker():
                                'js/service-worker.js', mimetype='text/javascript')
                                   
 @app.route('/api/add/<new_date>', methods=['GET'])
+def add(new_date):
+    ndate = addDate(new_date)
+    return jsonify(data=str(ndate), success=True) 
+
 def addDate(new_date):
     days = ["ראשון",
     "שני",
@@ -90,10 +94,8 @@ def addDate(new_date):
     ndate = dt( int(nds[0]), int(nds[1]), int(nds[2]) )
     comment = "יום {}".format(days[ndate.isoweekday()])
     mdb['operations'].insert([{"date":ndate, "comment":comment, "participants":[]}])
-    return jsonify(data=str(ndate), success=True) 
+    return ndate
 
-
-    
 @app.route('/api/delete/<del_date>', methods=['GET'])
 def deleteOperation(del_date):
     nds = del_date.split("-") #new_date split
@@ -101,6 +103,21 @@ def deleteOperation(del_date):
     mdb['operations'].remove({"date": ndate})
     return jsonify(data=str(ndate), success=True)
 
+@app.route('/api/batchdates/<start_date>', methods=['GET'])
+def batchdates(start_date):
+    nds = start_date.split("-") #new_date split
+    friday_one = dt( int(nds[0]), int(nds[1]), int(nds[2]) )
+    add_day = td(days = 1)
+    
+    response = []
+    for next_weekend in range(0,8):
+        weeks = td(weeks=next_weekend)
+        ndate = friday_one + weeks
+        response.append({ 'operation':addDate(ndate.strftime('%Y-%m-%d')) })
+    
+    
+    return jsonify(data=response, success=True)
+    
 @app.route('/api/list', methods=['POST', 'GET'])
 def apiOperations():
     col = mdb['operations']
