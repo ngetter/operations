aff65bf5344d3d8168be70387fce6a4d06c78156
Nir Getter <ngetter@gmail.com>
2018-09-04 09:25:44 +0000

    gobig

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn registration:app
+web: gunicorn flask run
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='scheduleme',
+    packages=['scheduleme'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
