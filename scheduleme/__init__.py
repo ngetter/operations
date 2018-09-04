51cba1be5f44e4fb2fd6dea29d8896c301323809
Nir Getter <ngetter@gmail.com>
2018-09-04 10:22:43 +0000

    template change

--- scheduleme/__init__.py
+++ scheduleme/__init__.py
@@ -122,22 +122,7 @@ def index():
 def addOperations():
     return render_template('add_operations.html')
     
-def getOperations(username):
-    col = mdb['operations']
-    l = list(col.find({'date': {'$gte': dt.now(None) - td(2)}}).sort("date", 1).limit(12))
 
-    for li in l:
-        try:
-
-            li['participants_count'] = len(li['participate'])
-            par_list = username in li
-            par_dict = [x for x in li['participate'] if isinstance(x, dict) and x['un'] == username]
-            li['participate'] = str((len(par_dict) > 0) or par_list)
-            li['participant_comment'] = par_dict[0]['comment']
-        except:
-            pass
-    return l
-        
 @app.route('/signin', methods=['POST', 'GET'])
 def signin():
     if request.method == 'POST':
@@ -198,30 +183,6 @@ def register():
         else:
             logen.info('annonymous redirected to registration form')
             return render_template('register.html', form=form)
-
-
-def sendRegMessage(to, member, id, opdate):
-    con = mdb['operations']
-    r = con.find_one({'_id': ObjectId(id)})
-    try:
-        users = mdb['users'].find({"username": {"$in": r['participate']}})
-    except KeyError:
-        users = []
-    
-    mailtxt = render_template('registeToOperation.html', username=member, id=id, opdate=opdate,
-                                      users=list(users), server=RETURN_TO)
-    mailtxt = transform(html = mailtxt)
-    return requests.post(
-        "https://api.mailgun.net/v3/mail.ngc.org.il/messages",
-        auth=("api", os.getenv('MAILGUN_KEY','')),
-        files=[("inline", open("static/img/logo.png", "rb"))],
-        data={"from": "מערכת רישום לפעולה - מדנ <admin@mail.ngc.org.il>",
-              "to": to,
-              "subject": u"רישום לפעולה במרכז דאייה נגב [%s]" % member,
-              "text": u"נרשמת לפעולה במרכז הדאייה נגב ביום -  %s" % opdate,
-              "html": mailtxt,
-              "o:tag": "registration"
-        })
 
 @app.route('/api/getparticipants', methods=['POST'])
 @app.route('/participants/<ObjectId:id>')
@@ -446,6 +407,44 @@ def sendReminder():
                 # 'sat_date': r[1]['date'],
                 # 'err' : err})
 
+def getOperations(username):
+    col = mdb['operations']
+    l = list(col.find({'date': {'$gte': dt.now(None) - td(2)}}).sort("date", 1).limit(12))
+
+    for li in l:
+        try:
+
+            li['participants_count'] = len(li['participate'])
+            par_list = username in li
+            par_dict = [x for x in li['participate'] if isinstance(x, dict) and x['un'] == username]
+            li['participate'] = str((len(par_dict) > 0) or par_list)
+            li['participant_comment'] = par_dict[0]['comment']
+        except:
+            pass
+    return l
+
+def sendRegMessage(to, member, id, opdate):
+    con = mdb['operations']
+    r = con.find_one({'_id': ObjectId(id)})
+    try:
+        users = mdb['users'].find({"username": {"$in": r['participate']}})
+    except KeyError:
+        users = []
+    
+    mailtxt = render_template('registeToOperation.html', username=member, id=id, opdate=opdate,
+                                      users=list(users), server=RETURN_TO)
+    mailtxt = transform(html = mailtxt)
+    return requests.post(
+        "https://api.mailgun.net/v3/mail.ngc.org.il/messages",
+        auth=("api", os.getenv('MAILGUN_KEY','')),
+        files=[("inline", open("static/img/logo.png", "rb"))],
+        data={"from": "מערכת רישום לפעולה - מדנ <admin@mail.ngc.org.il>",
+              "to": to,
+              "subject": u"רישום לפעולה במרכז דאייה נגב [%s]" % member,
+              "text": u"נרשמת לפעולה במרכז הדאייה נגב ביום -  %s" % opdate,
+              "html": mailtxt,
+              "o:tag": "registration"
+        })
 
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for x in range(size))
